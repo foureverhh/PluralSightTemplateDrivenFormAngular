ddd7f2141602587eaed4f50eab78d33a66088674
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2022-08-04 21:55:45 +0200

    add user-settings interface as data model for property in component

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserSettings } from '../model/user-settings';
 @Component({
   selector: 'app-user-settings-form',
   templateUrl: './user-settings-form.component.html',
   styleUrls: ['./user-settings-form.component.css']
 })
 export class UserSettingsFormComponent implements OnInit {
+  userSettings: UserSettings = {
+    name: 'Milton',
+    emailOffers: true, 
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annually',
+    note: 'Here are some notes...',
+  };
 
   constructor() { }
 
